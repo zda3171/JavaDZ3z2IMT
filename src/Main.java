985e3f563dc9d33cd8d660d8ff9d62740f8bef18
D.Zubkov <zda3171@gmail.com>
2022-05-25 20:42:11 +0300

    name

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double m = 120; // m-масса тела человека в кг
-        double h = 1.9; // h- рост человека в м
-        double bmi = service.calculate(m, h);
+        double mass = 120; // m-масса тела человека в кг
+        double height = 1.9; // h-рост человека в м
+        double bmi = service.calculate(mass, height);
         System.out.println("Ваш индекс массы тела =" + bmi);
     }
 }
