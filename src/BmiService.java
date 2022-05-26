a328143ff131e8aeccdc1732beacdc71f8e3e62a
D.Zubkov <zda3171@gmail.com>
2022-05-26 20:02:14 +0300

    format

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double mass, double height) {
-        double result = mass/(height*height); // mass-масса тела в кг, height-рост человека в м
+    public double calculate(double mass, double height) {
+        double result = mass / (height * height); // mass-масса тела в кг, height-рост человека в м
         return result;
     }
 
